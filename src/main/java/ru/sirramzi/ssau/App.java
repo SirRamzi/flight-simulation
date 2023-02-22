c6c33e7b457a5ea8531a69288f4bac86b6c5bb29
SirRamzi <aleks.aleksp@gmail.com>
2023-02-22 14:31:28 +0400

    Add trajetory for solar sail taking into account the solar wind

--- src/main/java/ru/sirramzi/ssau/App.java
+++ src/main/java/ru/sirramzi/ssau/App.java
@@ -12,8 +12,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,19 +23,18 @@ public class App extends Application {
     static double r0 = 1;
     static double mu = 1;
     static double rk = 1.524;
-    static double rz = 149.6 * (Math.pow(10, 9));
+    static double Rz = 149.6 * (Math.pow(10, 9));
     static double muSun = 1.3273 * (Math.pow(10, 20));
     static double deltaVFi = Math.sqrt((2 * rk) / (r0 + rk) * (1 / r0)) - Math.sqrt(1 / r0);
     static double Vfi0 = Math.sqrt(mu / r0);
     static double Vr0 = 0;
-    static double Pa = 9.1 * (Math.pow(10, -6));
+    static double Pa = 9.1 * (Math.pow(10, -1));
     static double Pa2 = 9.1 * (Math.pow(10, -4));
     static double Sm = 250;
     static double m = 450;
-    static double aSun = ((Pa * Sm) / (r0 * r0 * m)) * ((Math.pow(rz, 2)) / muSun);
-    static double aSun2 = ((Pa2 * Sm) / (r0 * r0 * m)) * (Math.pow(rz, 2) / muSun);
     static double lam = 0;
     private static double[] y0 = { r0, fi0, Vr0, Vfi0 + deltaVFi };
+    private static double[] y0Sun = { r0, fi0, Vr0, Vfi0 + deltaVFi };
 
     private LineChart<Number, Number> lineChartDec;
     private LineChart<Number, Number> lineChartPolar;
@@ -46,7 +43,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         double t0 = 0.0; // start time
         double tn = 4.4; // end time
-        double h = 0.1; // step size
+        double h = 0.01; // step size
 
         Function<Double, double[]> f = t -> {
             double[] dydt = new double[4];
@@ -54,6 +51,19 @@ public class App extends Application {
             dydt[1] = y0[3] / y0[0]; // dFiDt
             dydt[2] = Math.pow(y0[3], 2) / y0[0] - 1 / Math.pow(y0[0], 2); // dVrDt
             dydt[3] = -(y0[2] * y0[3]) / y0[0]; // dVFiDt
+            return dydt;
+        };
+
+        Function<Double, double[]> fSun = t -> {
+            double[] dydt = new double[4];
+            dydt[0] = y0Sun[2]; // drDt
+            dydt[1] = y0Sun[3] / y0Sun[0]; // dFiDt
+            dydt[2] = (Math.pow(y0Sun[3], 2) / y0Sun[0] - mu / Math.pow(y0Sun[0], 2))
+                    + ((((Pa * Sm * Math.pow(Rz, 2) * Math.pow(Math.cos(Math.toRadians(lam)), 2))
+                            / (Math.pow(y0Sun[0], 2) * m * mu))) * Math.cos(Math.toRadians(lam))); // dVrDt
+            dydt[3] = -((y0Sun[2] * y0Sun[3]) / y0Sun[0])
+                    + ((((Pa * Sm * Math.pow(Rz, 2) * Math.pow(Math.cos(Math.toRadians(lam)), 2))
+                    / (Math.pow(y0Sun[0], 2) * m * mu))) * Math.sin(Math.toRadians(lam))); // dVFiDt
             return dydt;
         };
 
@@ -65,9 +75,19 @@ public class App extends Application {
         dVrDtSeries.setName("dVr_dt");
         XYChart.Series<Number, Number> dVFiDtSeries = new XYChart.Series<>();
         dVFiDtSeries.setName("dVFi_dt");
-
         XYChart.Series<Number, Number> drDFiSeries = new XYChart.Series<>();
-        dVFiDtSeries.setName("dr_dFi");
+        drDFiSeries.setName("dr_dFi");
+
+        XYChart.Series<Number, Number> drDtSunSeries = new XYChart.Series<>();
+        drDtSunSeries.setName("dr_dtSun");
+        XYChart.Series<Number, Number> dFiDtSunSeries = new XYChart.Series<>();
+        dFiDtSunSeries.setName("dFi_dtSun");
+        XYChart.Series<Number, Number> dVrDtSunSeries = new XYChart.Series<>();
+        dVrDtSunSeries.setName("dVr_dtSun");
+        XYChart.Series<Number, Number> dVFiDtSunSeries = new XYChart.Series<>();
+        dVFiDtSunSeries.setName("dVFi_dtSun");
+        XYChart.Series<Number, Number> drDFiSunSeries = new XYChart.Series<>();
+        drDFiSunSeries.setName("dr_dFiSun");
 
         XYChart.Series<Number, Number> earthSeries = new XYChart.Series<>();
         earthSeries.setName("earh");
@@ -75,19 +95,30 @@ public class App extends Application {
         marsSeries.setName("mars");
 
         for (double i = 0; i <= 360; i++) {
-            earthSeries.getData().add(new XYChart.Data<>(r0 * Math.cos(Math.toRadians(i)), r0 * Math.sin(Math.toRadians(i))));
-            marsSeries.getData().add(new XYChart.Data<>(rk * Math.cos(Math.toRadians(i)), rk * Math.sin(Math.toRadians(i))));
+            earthSeries.getData()
+                    .add(new XYChart.Data<>(r0 * Math.cos(Math.toRadians(i)), r0 * Math.sin(Math.toRadians(i))));
+            marsSeries.getData()
+                    .add(new XYChart.Data<>(rk * Math.cos(Math.toRadians(i)), rk * Math.sin(Math.toRadians(i))));
         }
 
         for (double t = t0; t <= tn; t += h) {
             double[] y = RungeKutta4.solve(y0, t, t + h, f);
+            double[] ySun = RungeKutta4.solve(y0Sun, t, t + h, fSun);
             y0 = y;
+            y0Sun = ySun;
             drDtSeries.getData().add(new XYChart.Data<>(t, y0[0]));
             dFiDtSeries.getData().add(new XYChart.Data<>(t, y0[1]));
             dVrDtSeries.getData().add(new XYChart.Data<>(t, y0[2]));
             dVFiDtSeries.getData().add(new XYChart.Data<>(t, y0[3]));
-
             drDFiSeries.getData().add(new XYChart.Data<>(y0[0] * Math.cos(y0[1]), y0[0] * Math.sin(y0[1])));
+
+            drDtSunSeries.getData().add(new XYChart.Data<>(t, y0Sun[0]));
+            dFiDtSunSeries.getData().add(new XYChart.Data<>(t, y0Sun[1]));
+            dVrDtSunSeries.getData().add(new XYChart.Data<>(t, y0Sun[2]));
+            dVFiDtSunSeries.getData().add(new XYChart.Data<>(t, y0Sun[3]));
+            drDFiSunSeries.getData()
+                    .add(new XYChart.Data<>(y0Sun[0] * Math.cos(y0Sun[1]), y0Sun[0] * Math.sin(y0Sun[1])));
+
         }
 
         NumberAxis xAxisDec = new NumberAxis();
@@ -112,9 +143,10 @@ public class App extends Application {
         lineChartPolar.setCreateSymbols(false);
         lineChartPolar.setAxisSortingPolicy(SortingPolicy.NONE);
         lineChartPolar.getData().add(drDFiSeries);
+        lineChartPolar.getData().add(drDFiSunSeries);
         lineChartPolar.getData().add(earthSeries);
         lineChartPolar.getData().add(marsSeries);
-        
+
         // create a BorderPane to hold the menu and the charts
         BorderPane borderPane = new BorderPane();
 
