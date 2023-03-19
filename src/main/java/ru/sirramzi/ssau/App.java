e265ec4762962b5ef15193429f9838d4c154e4f7
SirRamzi <aleks.aleksp@gmail.com>
2023-03-19 15:44:18 +0400

    Update runge

--- src/main/java/ru/sirramzi/ssau/App.java
+++ src/main/java/ru/sirramzi/ssau/App.java
@@ -28,22 +28,27 @@ public class App extends Application {
     static double deltaVFi = Math.sqrt((2 * rk) / (r0 + rk) * (1 / r0)) - Math.sqrt(1 / r0);
     static double Vfi0 = Math.sqrt(mu / r0);
     static double Vr0 = 0;
-    static double Pa1 = 9.1 * (Math.pow(10, -5));
-    static double Pa2 = 9.1 * (Math.pow(10, -4));
+    static double Pa1 = 9.1 * (Math.pow(10, -6));
+    static double Pa2 = 9.1 * (Math.pow(10, -4.5));
     static double Sm = 250;
     static double m = 450;
-    static double lam = 0;
+    static double lam = 35.26;
     private static double[] y0 = { r0, fi0, Vr0, Vfi0 + deltaVFi };
     private static double[] y0Sun = { r0, fi0, Vr0, Vfi0 + deltaVFi };
 
-    private LineChart<Number, Number> lineChartDec;
+    private LineChart<Number, Number> rtlineChartDec;
+    private LineChart<Number, Number> PalineChartDec;
     private LineChart<Number, Number> lineChartPolar;
 
     @Override
     public void start(Stage stage) throws IOException {
         double t0 = 0.0; // start time
         double tn = 4.4; // end time
+        double t1 = 0.5;
+        double t2 = 4.2;
         double h = 0.01; // step size
+        double[] y;
+        double[] ySun;
 
         Function<Double, double[]> f = t -> {
             double[] dydt = new double[4];
@@ -54,30 +59,16 @@ public class App extends Application {
             return dydt;
         };
 
-        Function<Double, double[]> fSun = t -> {
-            double[] dydt = new double[4];
-            dydt[0] = y0Sun[2]; // drDt
-            dydt[1] = y0Sun[3] / y0Sun[0]; // dFiDt
-            dydt[2] = Math.pow(y0Sun[3], 2) / y0Sun[0] - mu / Math.pow(y0Sun[0], 2)
-                    + ((Pa1 * Sm) / (y0Sun[0] * y0Sun[0] * m)) * ((Rz * Rz) / muSun)
-                            * Math.pow(Math.cos(Math.toRadians(lam)), 2) * Math.cos(Math.toRadians(lam)); // dVrDt
-            dydt[3] = (-(y0Sun[2] * y0Sun[3]) / y0Sun[0])
-                    + ((Pa1 * Sm) / (y0Sun[0] * y0Sun[0] * m)) * ((Rz * Rz) / muSun)
-                            * Math.pow(Math.cos(Math.toRadians(lam)), 2) * Math.sin(Math.toRadians(lam)); // dVFiDt
-            return dydt;
-        };
-
         XYChart.Series<Number, Number> drDFiSeries = new XYChart.Series<>();
-        drDFiSeries.setName("dr_dFi");
+        drDFiSeries.setName("r_Fi");
         XYChart.Series<Number, Number> drDFiSunSeries = new XYChart.Series<>();
-        drDFiSunSeries.setName("dr_dFiSun");
+        drDFiSunSeries.setName("r_Fi_Sun");
         XYChart.Series<Number, Number> drDtSeries = new XYChart.Series<>();
-        drDtSeries.setName("dr_dt");
+        drDtSeries.setName("r_t");
         XYChart.Series<Number, Number> drDtSunSeries = new XYChart.Series<>();
-        drDtSunSeries.setName("dr_dtSun");
+        drDtSunSeries.setName("r_t_Sun");
         XYChart.Series<Number, Number> PaSeries = new XYChart.Series<>();
         PaSeries.setName("Pa");
-
         XYChart.Series<Number, Number> earthSeries = new XYChart.Series<>();
         earthSeries.setName("earh");
         XYChart.Series<Number, Number> marsSeries = new XYChart.Series<>();
@@ -91,37 +82,57 @@ public class App extends Application {
         }
 
         for (double t = t0; t <= tn; t += h) {
-            double[] y = RungeKutta4.solve(y0, t, t + h, f);
-            double[] ySun = RungeKutta4.solve(y0Sun, t, t + h, fSun);
+            double Pa = -((4 * Pa1 - 4 * Pa2) / (3 * t0 * t0 + 2 * t0 * tn - tn * tn)) * t * t
+                    - ((4 * Pa1 - 4 * Pa2) / (tn - 3 * t0)) * t
+                    - ((Pa1 * tn * tn - 3 * Pa1 * t0 * t0 + 2 * Pa1 * t0 * tn - 4 * Pa2 * t0 * tn)
+                            / (3 * t0 * t0 + 2 * t0 * tn - tn * tn));
+            Function<Double, double[]> fSun = tt -> {
+                double[] dydt = new double[4];
+                dydt[0] = y0Sun[2]; // drDt
+                dydt[1] = y0Sun[3] / y0Sun[0]; // dFiDt
+                dydt[2] = Math.pow(y0Sun[3], 2) / y0Sun[0] - mu / Math.pow(y0Sun[0], 2)
+                        + ((Pa * Sm) / (y0Sun[0] * y0Sun[0] * m)) * ((Rz * Rz) / muSun)
+                                * Math.pow(Math.cos(Math.toRadians(lam)), 2) * Math.cos(Math.toRadians(lam)); // dVrDt
+                dydt[3] = (-(y0Sun[2] * y0Sun[3]) / y0Sun[0])
+                        + ((Pa * Sm) / (y0Sun[0] * y0Sun[0] * m)) * ((Rz * Rz) / muSun)
+                                * Math.pow(Math.cos(Math.toRadians(lam)), 2) * Math.sin(Math.toRadians(lam)); // dVFiDt
+                return dydt;
+            };
+
+            y = RungeKutta4.solve(y0, t, t + h, f);
+            ySun = RungeKutta4.solve(y0Sun, t, t + h, fSun);
             y0 = y;
             y0Sun = ySun;
 
+            PaSeries.getData().add(new XYChart.Data<>(t, Pa));
             drDFiSeries.getData().add(new XYChart.Data<>(y0[0] * Math.cos(y0[1]), y0[0] * Math.sin(y0[1])));
             drDFiSunSeries.getData()
                     .add(new XYChart.Data<>(y0Sun[0] * Math.cos(y0Sun[1]), y0Sun[0] * Math.sin(y0Sun[1])));
-
             drDtSeries.getData().add(new XYChart.Data<>(t, y0[0]));
             drDtSunSeries.getData().add(new XYChart.Data<>(t, y0Sun[0]));
 
-            PaSeries.getData()
-                    .add(new XYChart.Data<>(t,
-                            -((4 * Pa1 - 4 * Pa2) / (3 * t0 * t0 + 2 * t0 * tn - tn * tn)) * t * t
-                                    - ((4 * Pa1 - 4 * Pa2) / (tn - 3 * t0)) * t
-                                    - ((Pa1 * tn * tn - 3 * Pa1 * t0 * t0 + 2 * Pa1 * t0 * tn - 4 * Pa2 * t0 * tn)
-                                            / (3 * t0 * t0 + 2 * t0 * tn - tn * tn))));
         }
 
-        NumberAxis xAxisDec = new NumberAxis();
-        NumberAxis yAxisDec = new NumberAxis();
-        xAxisDec.setLabel("Time, s");
-        yAxisDec.setLabel("Value");
-
-        lineChartDec = new LineChart<Number, Number>(xAxisDec, yAxisDec);
-        lineChartDec.setCreateSymbols(true);
-        lineChartDec.setAxisSortingPolicy(SortingPolicy.NONE);
-        lineChartDec.getData().add(PaSeries);
+        NumberAxis PaxAxisDec = new NumberAxis();
+        NumberAxis PayAxisDec = new NumberAxis();
+        PaxAxisDec.setLabel("Time, s");
+        PayAxisDec.setLabel("Pa");
+        PalineChartDec = new LineChart<Number, Number>(PaxAxisDec, PayAxisDec);
+        PalineChartDec.setCreateSymbols(false);
+        PalineChartDec.setAxisSortingPolicy(SortingPolicy.NONE);
+        PalineChartDec.getData().add(PaSeries);
         // lineChartDec.getData().add(drDtSeries);
         // lineChartDec.getData().add(drDtSunSeries);
+
+        NumberAxis rtxAxisDec = new NumberAxis();
+        NumberAxis rtyAxisDec = new NumberAxis();
+        rtxAxisDec.setLabel("Time, s");
+        rtyAxisDec.setLabel("r, ae");
+        rtlineChartDec = new LineChart<Number, Number>(rtxAxisDec, rtyAxisDec);
+        rtlineChartDec.setCreateSymbols(false);
+        rtlineChartDec.setAxisSortingPolicy(SortingPolicy.NONE);
+        rtlineChartDec.getData().add(drDtSeries);
+        rtlineChartDec.getData().add(drDtSunSeries);
 
         NumberAxis xAxisPolar = new NumberAxis(-2, 2, 0.25);
         NumberAxis yAxisPolar = new NumberAxis(-2, 2, 0.25);
@@ -145,31 +156,41 @@ public class App extends Application {
         vBox.setSpacing(10);
 
         // create the navigation buttons
+        Button rtChartButton = new Button("rt Chart");
+        Button PaChartButton = new Button("Pa Chart");
         Button polarChartButton = new Button("Polar Chart");
-        Button decChartButton = new Button("Decart Chart");
 
         // add the buttons to the VBox
-        vBox.getChildren().addAll(polarChartButton, decChartButton);
+        vBox.getChildren().addAll(rtChartButton, PaChartButton, polarChartButton);
 
         // create the StackPane to hold the charts
         StackPane stackPane = new StackPane();
         stackPane.setAlignment(Pos.CENTER);
 
         // add the line chart and the bar chart to the StackPane
-        stackPane.getChildren().addAll(lineChartDec, lineChartPolar);
+        stackPane.getChildren().addAll(rtlineChartDec, PalineChartDec, lineChartPolar);
 
         // set the visibility of the bar chart to false
         lineChartPolar.setVisible(false);
+        PalineChartDec.setVisible(false);
 
-        // set the action for the line button
-        polarChartButton.setOnAction((event) -> {
-            lineChartDec.setVisible(true);
+        rtChartButton.setOnAction((event) -> {
+            rtlineChartDec.setVisible(true);
+            PalineChartDec.setVisible(false);
             lineChartPolar.setVisible(false);
         });
 
+        // set the action for the line button
+        PaChartButton.setOnAction((event) -> {
+            rtlineChartDec.setVisible(false);
+            PalineChartDec.setVisible(true);
+            lineChartPolar.setVisible(false);
+        });
+        
         // set the action for the bar button
-        decChartButton.setOnAction((event) -> {
-            lineChartDec.setVisible(false);
+        polarChartButton.setOnAction((event) -> {
+            rtlineChartDec.setVisible(false);
+            PalineChartDec.setVisible(false);
             lineChartPolar.setVisible(true);
         });
 
